ec5bf45e577fa644ad7d9bf23d3e218d8648a17b
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2013-09-27 06:57:01 +0900

    update latest deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,8 @@
 require 'mina/git'
+require 'mina/bundler'
 require 'mina/rbenv'
 
-set :domain, "133.242.17.37"
+set :domain, "www.hsbt.org"
 set :user, "hsbt"
 set :deploy_to, "/home/#{user}/app/tdiary"
 set :repository, 'git://github.com/tdiary/tdiary-core.git'
@@ -11,20 +12,29 @@ task :environment do
   invoke :'rbenv:load'
 end
 
-desc 'update shared library'
-task :update_library => :environment do
+task :copy_assets do
   queue "cp -r #{deploy_to}/#{shared_path}/lib/* #{deploy_to}/#{current_path}/misc/lib"
   queue "cp -r #{deploy_to}/#{shared_path}/js/* #{deploy_to}/#{current_path}/js"
+  queue "sudo cp -r #{deploy_to}/#{current_path}/theme/* /var/www/hsbt.org/diary/theme"
 end
 
-desc "Deploys the current version to the server."
+task :generate_gemfile_local do
+  queue "echo \"gem 'tdiary-style-gfm'\" > #{deploy_to}/#{current_path}/Gemfile.local"
+end
+
+task :restart do
+  queue 'sudo /etc/init.d/apache2 restart'
+end
+
 task :deploy => :environment do
   deploy do
     invoke :'git:clone'
+    invoke :generate_gemfile_local
+    invoke :'bundle:install'
     invoke :'deploy:cleanup'
 
     to :launch do
-      invoke :update_library
+      invoke :copy_assets
     end
   end
 end

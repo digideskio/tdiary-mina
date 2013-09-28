ca38ce41af8f1ba2e7b6080ad518e04ae49a01e6
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2013-09-28 13:22:33 +0900

    update deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,4 @@
 require 'mina/git'
-require 'mina/bundler'
 require 'mina/rbenv'
 
 set :domain, "www.hsbt.org"
@@ -12,14 +11,18 @@ task :environment do
   invoke :'rbenv:load'
 end
 
-task :copy_assets do
+task :copy_assets => :environment do
   queue "cp -r #{deploy_to}/#{shared_path}/lib/* #{deploy_to}/#{current_path}/misc/lib"
   queue "cp -r #{deploy_to}/#{shared_path}/js/* #{deploy_to}/#{current_path}/js"
   queue "sudo cp -r #{deploy_to}/#{current_path}/theme/* /var/www/hsbt.org/diary/theme"
 end
 
-task :generate_gemfile_local do
+task :generate_gemfile_local => :environment do
   queue "echo \"gem 'tdiary-style-gfm'\" > #{deploy_to}/#{current_path}/Gemfile.local"
+end
+
+task :bundle => :environment do
+  queue "cd #{deploy_to}/#{current_path}; bundle install --without development:test"
 end
 
 task :restart do
@@ -29,12 +32,13 @@ end
 task :deploy => :environment do
   deploy do
     invoke :'git:clone'
-    invoke :generate_gemfile_local
-    invoke :'bundle:install'
     invoke :'deploy:cleanup'
 
     to :launch do
+      invoke :generate_gemfile_local
       invoke :copy_assets
+      invoke :bundle
+      invoke :restart
     end
   end
 end
